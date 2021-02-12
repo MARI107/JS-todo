2ccf547af211b5de0421a490e2eb88539fb94901
mari107 <62844176+MARI107@users.noreply.github.com>
2021-02-12 14:24:41 +0900

    完了したTODOも戻す前の処理まで

--- src/index.js
+++ src/index.js
@@ -18,12 +18,33 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    //完了リストに追加する要素
-    const addTarget = completeButton.parentNode;
-    const text = addTarget.firstElementChild.innerText;
-    console.log(text);
     //押された完了bボタンの親タグ（div）を未完了リストから削除
     deleteFromIncompleteList(deleteButton.parentNode);
+    //完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    //TODOの内容テキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    //div以下を初期化
+    addTarget.textContent = null;
+    //liタグの生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    //buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      alert("もどス");
+    });
+
+    //divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    //完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   //button(削除)タグの生成
