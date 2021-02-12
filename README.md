b13369f69b984af70a17d2f526f22a7c421e2365
mari107 <62844176+MARI107@users.noreply.github.com>
2021-02-12 15:04:25 +0900

    TODOリストが完成！

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# JS-todo
+# JavaScript-todo
+
 Created with CodeSandbox
+
+JavaScript を使って、Todo リストを作りました。
+一番上の TODO を入力を押すと未完了の TODO に入ります。
+完了を押すと完了した TODO に入ります。
+戻す事も削除することも可能です。
--- src/index.js
+++ src/index.js
@@ -5,14 +5,23 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteLIst(inputText);
+};
+
+//未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+//未完了リストに追加する関数
+const createIncompleteLIst = (text) => {
   //div生成
   const div = document.createElement("div");
   div.className = "list-row";
   console.log(div);
-
   //liタグ生成
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   //button(完了)タグの生成
   const completeButton = document.createElement("button");
@@ -36,7 +45,12 @@ const onClickAdd = () => {
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
     backButton.addEventListener("click", () => {
-      alert("もどス");
+      //押された戻すボタンの親タグ（div)を完了リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+      //テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteLIst(text);
     });
 
     //divタグの子要素に各要素を設定
@@ -65,12 +79,6 @@ const onClickAdd = () => {
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
 };
-
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
-
-//未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
-};
